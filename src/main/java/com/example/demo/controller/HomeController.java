448da520eb240e0a582df768e997cf05c7504ed4
jamsubzero <jamsubzero@gmail.com>
2019-02-18 14:53:23 +0800

    edited new controller

---
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -0,0 +1,15 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HomeController {
+
+	public String home() {
+		return "welcome to my spring demo";
+	}
+	
+	
+}
